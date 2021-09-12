2dd223be09bd9536698da6c86ae05b31dd2104fa
Valdemar322 <lavrovvoldemar@gmail.com>
2021-09-12 17:05:45 +0300

    add header

--- index.php
+++ index.php
@@ -1,24 +1,43 @@
-<?php
-//PDO
-// $connection = new PDO("mysql: host=localhost; dbname=test; charset=utf8", "root","");
-// $query = "insert into users (name, age, login, password) values ('Roma','10','dovakin','123123aa')";
-// $connection->exec($query);
-$connection = new PDO("mysql: host=localhost; dbname=test; charset=utf8","root","");
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Urbanist:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <title>Blog</title>
+</head>
+<body>
+<header class="container-fluid">
+    <div class="container">
+        <div class="row">
+            <div class="col-4">
+                <h1><a href="/">My blog</a></h1>
+            </div>
+            <nav class="col-8">
+                <ul>
+                    <li><a href="">Главная</a></li>
+                    <li><a href="">О нас</a></li>
+                    <li><a href="">Услуги</a></li>
+                    <li>
+                        <a href=""><i class="fas fa-user"></i> Кабинет</a>
+                        <ul>
+                            <li><a href="">Админ панель</a></li>
+                            <li><a href="">Выход</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</header>
 
-$name = "Vasya";
-$age = 30;
-$login = 'abbr';
-$password = "alalala";
 
-$param = [
-'name' => $name,
-'age' => $age,
-'login' => $login,
-'password' => $password
-];
-
-$sql = "INSERT INTO users (name,age,login,password) VALUES (:name, :age, :login, :password)";
-$query = $connection->prepare($sql);
-
-$query->execute($param);
-?>
+<script src="https://kit.fontawesome.com/6b3d557910.js" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+</body>
+</html>
